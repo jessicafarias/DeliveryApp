179eb601ede69b769cf187e3522920cecc9d256f
jessica <jessica.farias.rosado@gmail.com>
2020-08-05 16:20:32 -0500

    Adding MySql database

---
+++ app/src/main/java/com/example/godrive/DB.java
@@ -0,0 +1,83 @@
+package com.example.godrive;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class DB extends _Default implements Runnable {
+
+    private Connection conn;
+    private String host = "fdb24.awardspace.net";
+    private String db = "3328198_db";
+    private int port = 3306;
+    private String user = "3328198_db";
+    private String pass = "125789jJ";
+    private String url = "jdbc:postgresql://%s:%d/%s";
+
+    public DB() {
+        super();
+        this.url = String.format(this.url,this.host, this.port, this.db);
+
+        this.conecta();
+        this.disconecta();
+    }
+
+    @Override
+    public void run() {
+        try{
+            Class.forName("org.postgresql.Driver");
+            this.conn = DriverManager.getConnection(this.url,this.user,this.pass);
+        }catch (Exception e){
+            this._mensagem = e.getMessage();
+            this._status = false;
+        }
+    }
+
+    private void conecta(){
+        Thread thread = new Thread(this);
+        thread.start();
+        try{
+            thread.join();
+        }catch (Exception e){
+            this._mensagem = e.getMessage();
+            this._status = false;
+        }
+    }
+
+    public void disconecta(){
+        if (this.conn!= null){
+            try{
+                this.conn.close();
+            }catch (Exception e){
+
+            }finally {
+                this.conn = null;
+            }
+        }
+    }
+
+    public ResultSet select(String query){
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
+    }
+
+    public ResultSet execute(String query){
+        this.conecta();
+        ResultSet resultSet = null;
+        try {
+            resultSet = new ExecuteDB(this.conn, query).execute().get();
+        }catch (Exception e){
+            this._status = false;
+            this._mensagem = e.getMessage();
+        }
+        return resultSet;
+    }
+
+}
