6e03b593c42a9e1fde7eb9d589a4a0003dd15cc0
jessica <jessica.farias.rosado@gmail.com>
2020-08-05 14:19:33 -0500

    Adding db firebox
    Adding firebase
    Adding google plugins
    Adding new activity_main

---
+++ app/src/main/java/com/example/godrive/main.java
@@ -0,0 +1,14 @@
+package com.example.godrive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
