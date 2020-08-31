5ebd4c4dc2839b451431ead44c4c6d95b5c01ebd
jessica <jessica.farias.rosado@gmail.com>
2020-08-30 22:14:32 -0500

    Toat.show the distance bewteen 2 points

--- app/src/main/java/com/example/godrive/MapsActivity.java
+++ app/src/main/java/com/example/godrive/MapsActivity.java
@@ -24,6 +24,8 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -70,10 +72,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         LatLng sydney = getLocation();
+        LatLng sydney2 = new LatLng(21.0007277, -89.7047104);
         //LatLng sydney = new LatLng(getLocation().latitude, getLocation().longitude);
         //LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("YOU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,13));
+        mMap.addMarker(new MarkerOptions().position(sydney2).title("DESTINATION"));
+        CircleOptions circleOptions = new CircleOptions()
+                .center(sydney)
+                .radius(1000); // In meters
+        // Get back the mutable Circle
+        Circle circle = mMap.addCircle(circleOptions);
+
+        Location locA = new Location("punto A");
+        Location locB = new Location("punto B");
+        locA.setLatitude(sydney.latitude);
+        locA.setLongitude(sydney.longitude);
+        locB.setLatitude(sydney2.latitude);
+        locB.setLongitude(sydney2.longitude);
+        float dis = locA.distanceTo(locB);
+
+        Toast.makeText(this, ""+dis+"", Toast.LENGTH_LONG).show();
     }
 
     public LatLng getLocation() {
