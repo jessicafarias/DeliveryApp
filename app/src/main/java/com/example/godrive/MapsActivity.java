f050921d14e8bad067dc96faed2ec58351686eac
jessica <jessica.farias.rosado@gmail.com>
2020-09-23 17:30:01 -0500

    find and set new location

--- app/src/main/java/com/example/godrive/MapsActivity.java
+++ app/src/main/java/com/example/godrive/MapsActivity.java
@@ -11,10 +11,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.provider.Settings;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -39,27 +44,39 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.List;
+
 
 //public class CurrentlocationActivity extends AppCompatActivity {
 public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarkerDragListener, OnMapReadyCallback {
 
-    private GoogleMap mMap;
+    private GoogleMap mMap, mMap2;
     private static  final int REQUEST_LOCATION=1;
     float dist;
 
-    private RequestQueue queue;
+    LatLng sydney2, sydney, jmf;
 
+    private RequestQueue queue;
 
     LocationManager locationManager;
     String latitude,longitude;
 
-    private Marker origin, destination, driver;
+    private Marker origin, destination, driver, extra;
 
 
+    private Button BtnMarkerDestination;
+    private EditText EditTextfind;
+
+    //
+
+    //
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        BtnMarkerDestination = (Button)findViewById(R.id.BtnMarkerDestination);
+        EditTextfind = (EditText)findViewById(R.id.editTextfind);
 
         ////////////////////////////////// Instantiate the RequestQueue.
        queue = Volley.newRequestQueue(this);
@@ -76,6 +93,20 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        BtnMarkerDestination.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    //GetGeoName();
+
+                    ChangeDestination();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
     public void ButtonAction() {
@@ -88,17 +119,59 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
             getLocation();
         }
     }
+
+    public void GetGeoName() throws IOException {
+
+        Geocoder geo = new Geocoder(this);
+        List<Address> adress = null;
+        try {
+            adress = geo.getFromLocationName(EditTextfind.getText().toString(),1,
+                    1,1,1,1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        LatLng jmf = new LatLng(adress.get(0).getLatitude(), adress.get(0).getLongitude());
+
+        extra= mMap.addMarker(new MarkerOptions().position(jmf).icon(BitmapDescriptorFactory.fromResource(R.drawable.car)));
+    }
+
+    public void ChangeDestination() throws IOException {
+        Geocoder geo = new Geocoder(this);
+        List<Address> adress = null;
+        try {
+            adress = geo.getFromLocationName(EditTextfind.getText().toString(),1,
+                    1,1,1,1);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+        if (adress.size() >0){
+            jmf = new LatLng(adress.get(0).getLatitude(), adress.get(0).getLongitude());
+            destination.setPosition(jmf);
+        }
+        else{
+            Toast.makeText(this,"No se encontró resultado", Toast.LENGTH_SHORT).show();
+        }
+
+
+        //destination= mMap.addMarker(new MarkerOptions().position(jmf).draggable(true).title("DESTINO"));
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        LatLng sydney = getLocation();
-        LatLng sydney2 = new LatLng(sydney.latitude+0.01, sydney.longitude);
+        sydney = getLocation();
+        if (sydney2==null){
+            sydney2 = new LatLng(sydney.latitude+0.01, sydney.longitude);
+        }
         //LatLng sydney = new LatLng(getLocation().latitude, getLocation().longitude);
         //LatLng sydney = new LatLng(-34, 151);
         origin= mMap.addMarker(new MarkerOptions().position(sydney).title("YOU").draggable(true).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,13));
-        destination= mMap.addMarker(new MarkerOptions().position(sydney2).draggable(true).title("DESTINO"));
+
         driver= mMap.addMarker(new MarkerOptions().position(sydney2).icon(BitmapDescriptorFactory.fromResource(R.drawable.car)));
+        destination= mMap.addMarker(new MarkerOptions().position(sydney2).draggable(true).title("DESTINO"));
+
 
         CircleOptions circleOptions = new CircleOptions()
                 .center(sydney)
@@ -107,28 +180,15 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         Circle circle = mMap.addCircle(circleOptions);
         mMap.setOnMarkerDragListener(this);
 
-        Location locA = new Location("punto A");
-        Location locB = new Location("punto B");
-        locA.setLatitude(origin.getPosition().latitude);
-        locA.setLongitude(origin.getPosition().longitude);
-        locB.setLatitude(destination.getPosition().latitude);
-        locB.setLongitude(destination.getPosition().longitude);
-        dist = locA.distanceTo(locB);
 
+        //Location locA = new Location("punto A");
+        //Location locB = new Location("punto B");
+        //locA.setLatitude(origin.getPosition().latitude);
+        //locA.setLongitude(origin.getPosition().longitude);
+        //locB.setLatitude(destination.getPosition().latitude);
+        //locB.setLongitude(destination.getPosition().longitude);
+        //dist = locA.distanceTo(locB);
 
-        Toast.makeText(this, ""+dist+"", Toast.LENGTH_LONG).show();
-        //https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&mode=bicycling&language=fr-FR&key=AIzaSyB1e7npY-O5g3V7Erm4v3I9nM1wTLFBMw8
-        //jsonfile
-
-        //https://developers.google.com/maps/documentation/distance-matrix/overview
-
-        //Volley
-        //https://www.youtube.com/watch?v=YNM_-cR9QKQ
-        //
-        //JUST POLILINE
-        //https://www.youtube.com/watch?v=58AxNh2cWRU
-
-        //Extra: https://www.it-swarm.dev/es/git/como-descartar-cambios-locales-y-obtener-lo-ultimo-del-repositorio-de-github/827022313/
     }
 
     public LatLng getLocation() {
@@ -226,11 +286,12 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
         locB.setLatitude(destination.getPosition().latitude);
         locB.setLongitude(destination.getPosition().longitude);
         dist = locA.distanceTo(locB);
-        Toast.makeText(this, ""+dist+"", Toast.LENGTH_LONG).show();
+        //Toast.makeText(this, ""+dist+"", Toast.LENGTH_LONG).show();
+
     }
 
     private void JsonParse()    {
-        String url = "https://my-json-server.typicode.com/jessicafarias/DeliveryApp/db";
+        String url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=AIzaSyB1e7npY-O5g3V7Erm4v3I9nM1wTLFBMw8";
 
         /////////////////////////////// Request a string response from the provided URL.
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
@@ -238,12 +299,12 @@ public class MapsActivity extends FragmentActivity implements GoogleMap.OnMarker
                     @Override
                     public void onResponse(JSONObject response) {
                         try {
-                            JSONArray jsonarray =response.getJSONArray("empleado");
-                            for (int i =0 ; i< jsonarray.length(); i++){
-                                JSONObject empleado = jsonarray.getJSONObject(i);
-                                String name = empleado.getString("name");
-                                Toast.makeText(MapsActivity.this, ""+name+"", Toast.LENGTH_LONG).show();
-                            }
+                            //JSONArray jsonarray =response.getJSONArray("empleado");
+                            //for (int i =0 ; i< jsonarray.length(); i++){
+                                //JSONArray empleado = jsonarray.getJSONObject(i);
+                                String name = response.getString("error_message");
+                                //Toast.makeText(MapsActivity.this, ""+name+"", Toast.LENGTH_LONG).show();
+                            //}
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
